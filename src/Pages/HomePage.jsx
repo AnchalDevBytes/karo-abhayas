d0dbc5a1ed9e3c69bccc6c72c890b770c87be4d3
anchalraj31082004 <anchalraj20045@gmail.com>
2023-12-22 08:52:35 +0530

    fixed minor css

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -20,7 +20,21 @@ const HomePage = () => {
 
   return (
     <div className='md:px-9 pb-8 md:py-8 flex flex-col gap-7'>
-      <p className={`${isVisible ? "hidden" : "block"} animate-pulse absolute z-50 bg-stone-500/90 h-64 w-[450px] shadow-black/70 backdrop-filter backdrop-blur-sm shadow-xl text-white flex items-center justify-center rounded-lg ml-[35%] mt-[8%]`}>Please visit <Link className="text-sky-400 text-lg font-bold mx-5" to="/products"> Product </Link> and <Link to="/cart" className="text-sky-400 text-lg font-bold mx-5"> cart </Link> page</p>
+      <p
+        className={`${
+          isVisible ? 'hidden' : 'block'
+        } animate-pulse absolute z-50 bg-stone-500/90 h-64 w-[90%] md:w-[450px] shadow-black/70 backdrop-filter backdrop-blur-sm shadow-xl text-white flex items-center justify-center rounded-lg left-1/2 transform -translate-x-1/2 mt-5 md:mt-[8%]`}
+      >
+        Please visit{' '}
+        <Link className='text-sky-400 text-base md:text-lg font-bold mx-2' to='/products'>
+          Product
+        </Link>{' '}
+        and{' '}
+        <Link to='/cart' className='text-sky-400 text-base md:text-lg font-bold mx-2'>
+          cart
+        </Link>{' '}
+        page
+      </p>
       <Carousel images={images}/>
       <TopStudent/>
       <Review/>
